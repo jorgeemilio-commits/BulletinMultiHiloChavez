85d4829eda8179104a60e8d51d5be690ba3045f9
Jorge Emilio <jorge.emilio.chavez.lizarraga@gmail.com>
2025-10-01 07:36:41 -0700

    Se implemento la primera version del servidor

--- servidorhilos/src/main/java/com/servidormulti/ServidorMultiHilos.java
+++ servidorhilos/src/main/java/com/servidormulti/ServidorMultiHilos.java
@@ -1,7 +1,25 @@
 package com.servidormulti;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
 public class ServidorMultiHilos {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    static HashMap<String, UnCliente> clientes = new HashMap<String,UnCliente>();
+
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket servidorSocket = new ServerSocket(8080);
+        int contador = 0;
+        while (true) {
+            Socket s = servidorSocket.accept();
+            UnCliente unCliente = new UnCliente(s);
+            Thread hilo = new Thread(unCliente);
+            clientes.put(Integer.toString(contador) , unCliente);
+            hilo.start();
+            contador++;
+        }
     }
 }
--- servidorhilos/src/main/java/com/servidormulti/UnCliente.java
+++ servidorhilos/src/main/java/com/servidormulti/UnCliente.java
@@ -1,5 +1,38 @@
 package com.servidormulti;
 
-public class UnCliente {
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class UnCliente implements Runnable {
+    
+    final DataOutputStream salida;
+
+    final BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+
+    final DataInputStream entrada;
+
+    UnCliente(Socket s) throws java.io.IOException {
+        this.salida = new DataOutputStream(s.getOutputStream());
+        this.entrada = new DataInputStream(s.getInputStream());
+    }
+    
+
+    @Override
+    public void run() {
+        String mensaje;
+        while (true) {
+            try {
+                mensaje = entrada.readUTF();
+                for (UnCliente unCliente : ServidorMultiHilos.clientes.values()) {
+                    unCliente.salida.writeUTF(mensaje);
+                }
+            } catch (Exception x) {
+            }
+        }
+    }
     
 }
